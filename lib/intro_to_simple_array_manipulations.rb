895949a8fad9a8581ff0027cb3b17c071d80533a
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-14 16:19:16 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(fave_things, more_favs)
+  fave_things.concat (more_favs)
+end 
+
+def using_insert(array1, item)
+  array1.insert(4, item)
+end 
+
+def using_uniq(array)
+  array.uniq 
+end 
+
+def using_flatten(array)
+  array.flatten 
+end 
+
+def using_delete(array, string)
+  array.delete(string)
+end 
+
+def using_delete_at(array, name)
+  array.delete_at(name)
+end 
