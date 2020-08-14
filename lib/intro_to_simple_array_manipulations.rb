8e1da42c00f8aa36c703f65210f268062b8bb9e2
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-14 16:00:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,5 +2,6 @@ def using_concat(fave_things, more_favs)
   fave_things.concat (more_favs)
 end 
 
-def using_insert(array1, array2)
-  array1.
+def using_insert(array1, item)
+  array1.insert(4, item)
+end 
